2447df8fdee9a1445bec38e2c095ed79df74c1f8
dt-seneca <det_seneca.proton.me>
2022-05-22 20:10:55 +0200

    Finished styling.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,18 +1,20 @@
 import * as React from 'react'
-
+import {container, contactHeading} from './contact.module.css'
 const Contact = () => {
     return (
         <div>
-            <div>Contact</div>
-            <div>det_seneca@proton.me</div>
-            <form>
-                <div>Message Me</div>
-                <input type="text" placeholder="Name"/>
-                <input type="email" placeholder='Email'/>
-                <textarea>
-                </textarea>
-                <input type="submit" />
-            </form>
+            <h2 className={contactHeading}>Contact</h2>
+            <div className={container}>
+                <div>det_seneca@proton.me</div>
+                <form>
+                    <div>Message Me</div>
+                    <input type="text" placeholder="Name"/>
+                    <input type="email" placeholder='Email'/>
+                    <textarea>
+                    </textarea>
+                    <input type="submit" value="Send" />
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/education.js
+++ src/components/education.js
@@ -1,20 +1,17 @@
 import * as React from 'react'
-
+import {container, educationHeading} from './education.module.css'
 const Education = () => {
     return (
         <div>
-            <div>
+            <h2 className={educationHeading}>Education</h2>
+            <div className={container}>
                 <div>
-                    09/16 - 05/17
-                </div>
-                <div>
+                    <em>09/16 - 05/17</em><br />
                     Goldsmiths, University of London<br />
                     Foundation of Computing
                 </div>
                 <div>
-                    09/18 - 09/21
-                </div>
-                <div>
+                    <em>09/18 - 09/21</em><br />
                     Goldsmiths, University of London <br />
                     BSc in Computer Science (Withdrew)
                 </div>
