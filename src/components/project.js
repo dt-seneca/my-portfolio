e6e3b9ae4a64cf8bfa2b56f7fad2dcd8827cd087
dt-seneca <det_seneca.proton.me>
2022-05-22 18:22:03 +0200

    made a json file to easily store project names and technolgies. I still need to include project links and images.

--- src/components/project.js
+++ src/components/project.js
@@ -1,79 +1,37 @@
 import * as React from 'react';
-import {container, projectContainer} from './project.module.css';
+import data from '../project.json';
+import {projectHeading, container, projectContainer, projectTitle, projectImage, techList, buttonContainer} from './project.module.css';
+
+const ProjectDiv = (props) => {
+    return (
+        <div className={projectContainer}>
+                    <div className={projectTitle}>{props.name}</div>
+                    <div className={projectImage}>i</div>
+                    <ul className={techList}>
+                        {props.tech.map((techItem) => {
+                            console.log(techItem);
+                            return (<li>{techItem}</li>);
+                        })
+                        }
+                    </ul>
+                    <div className={buttonContainer}>
+                        <button>Source</button>
+                        <button>Demo</button>
+                    </div>
+                </div>
+    )
+}
+
 const Project = () => {
     return (
-        <div className={container}>
-            <div className={projectContainer}>
-                <div>Title</div>
-                <ul>
-                    <li>VueJS</li>
-                    <li>NodeJS</li>
-                    <li>MongoDB</li>
-                </ul>
-                <div>
-                    <button>Source</button>
-                    <button>Demo</button>
-                </div>
-            </div>
-            <div className={projectContainer}>
-                <div>Title</div>
-                <ul>
-                    <li>VueJS</li>
-                    <li>NodeJS</li>
-                    <li>MongoDB</li>
-                </ul>
-                <div>
-                    <button>Source</button>
-                    <button>Demo</button>
-                </div>
-            </div>
-            <div className={projectContainer}>
-                <div>Title</div>
-                <ul>
-                    <li>VueJS</li>
-                    <li>NodeJS</li>
-                    <li>MongoDB</li>
-                </ul>
-                <div>
-                    <button>Source</button>
-                    <button>Demo</button>
-                </div>
-            </div>
-            <div className={projectContainer}>
-                <div>Title</div>
-                <ul>
-                    <li>VueJS</li>
-                    <li>NodeJS</li>
-                    <li>MongoDB</li>
-                </ul>
-                <div>
-                    <button>Source</button>
-                    <button>Demo</button>
-                </div>
-            </div>
-            <div className={projectContainer}>
-                <div>Title</div>
-                <ul>
-                    <li>VueJS</li>
-                    <li>NodeJS</li>
-                    <li>MongoDB</li>
-                </ul>
-                <div>
-                    <button>Source</button>
-                    <button>Demo</button>
-                </div>
-            </div>
-            <div className={projectContainer}>
-                <div>Title</div>
-                <ul>
-                    <li>VueJS</li>
-                    <li>NodeJS</li>
-                    <li>MongoDB</li>
-                </ul>
-                <div>
-                    <button>Source</button>
-                    <button>Demo</button>
-                </div>
+        <div>
+            <h2 className={projectHeading}>Projects</h2>
+            <div className={container}>
+                {
+                    data.projects.map((projObj) => {
+                        return <ProjectDiv name={projObj.name} tech={projObj.technologies} />
+                    })
+                }
             </div>
         </div>
     )
